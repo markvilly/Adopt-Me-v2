0b9c082e263c45bb9edb92ec08eff95b340ab138
Mark Paul <markpaulbusiness66@gmail.com>
2024-12-29 11:21:55 +0300

    Breed options clears when animal changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const ANIMALS = ["dog", "bird", "cat"];
-  // const BREEDS: string[] = ["poodles"];
+  const ANIMALS = ["dog", "bird", "cat", "rabbit", "reptile"];
   const [animal, setAnimal] = useState("");
-  const [breed, setBreed] = useState([]);
+  const [location, setLocation] = useState("");
+  const [breed, setBreed] = useState("");
+  const BREEDS: string[] = ["poodles", "gazelle"];
 
   useEffect(() => {}, []);
 
+  console.log(breed);
   return (
     <>
       <div className=" border-gray-100 drop-shadow-sm w-4/12 m-auto bg-pink-100 p-10 rounded-md">
@@ -16,52 +18,55 @@ function App() {
           action=""
           onSubmit={(e) => {
             e.preventDefault();
-            console.log(animal);
           }}
         >
-          <div className=" flex flex-col justify-between my-2 mx-auto items-center w-2/3">
-            <label htmlFor="location">Location</label>
-            <input
-              type="text"
-              id="location"
-              className=" bg-white w-full p-2 rounded-sm  "
-            />
+          <div className=" px-2 flex my-2 flex-col">
+            <label htmlFor="location">
+              {" "}
+              Location
+              <input type="text" name="location" />
+            </label>
           </div>
-          <div className=" flex flex-col justify-between items-center">
-            <label htmlFor="anima">Animal</label>
-            <select
-              value={animal}
-              id="animal"
-              onChange={(e) => {
-                setAnimal(e.target.value);
-                setBreed([]);
-              }}
-              className="p-3 rounded-sm w-2/3"
-            >
-              <option value="" />
-              {ANIMALS.map((item, index) => (
-                <option key={index}>{item}</option>
-              ))}
-            </select>
+          <div className=" px-2 flex my-2 flex-col">
+            <label htmlFor="animal">
+              {" "}
+              Animal
+              <select
+                name=""
+                id=""
+                onChange={(e) => {
+                  setAnimal(e.target.value);
+                  setBreed(" ");
+                }}
+              >
+                <option />
+                {ANIMALS.map((animal) => (
+                  <option key={animal}>{animal}</option>
+                ))}
+              </select>
+            </label>
           </div>
-          <div className=" flex flex-col justify-between my-2 items-center">
-            <label htmlFor="breed">Breed</label>
-            <select
-              name=""
-              id=""
-              disabled={breed.length === 0}
-              className={`p-3 rounded-sm w-2/3 `}
-            >
-              <option value=""></option>
-              {breed.map((item, index) => (
-                <option value="" key={index} className="">
-                  {item}
-                </option>
-              ))}
-            </select>
+          <div className=" px-2 flex my-2 flex-col">
+            <label htmlFor="location">
+              {" "}
+              Breed
+              <select
+                name=""
+                id=""
+                className=" py-2 px-16 rounded-sm"
+                value={breed}
+                onChange={(e) => {
+                  setBreed(e.target.value);
+                }}
+              >
+                <option />
+                {BREEDS.map((breed, index) => (
+                  <option key={index}>{breed}</option>
+                ))}
+              </select>
+            </label>
           </div>
-
-          <button className=" bg-pink-400 py-2 px-4 rounded-md mt-6  text-white ">
+          <button className=" bg-pink-400 py-2 px-4 rounded-md mt-6  text-white">
             Submit
           </button>
         </form>
